35a6d39fdf91813cd214484f566cdeaeb751b95f
y2o2u2n <y2o2u2n@gmail.com>
2019-08-17 16:55:18 +0900

    Create & Delete, Read(paging)

--- src/main/java/com/example/app/rooms/RoomRepository.java
+++ src/main/java/com/example/app/rooms/RoomRepository.java
@@ -1,6 +1,6 @@
 package com.example.app.rooms;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface RoomRepository extends CrudRepository<Room, Long> {
+public interface RoomRepository extends PagingAndSortingRepository<Room, Long> {
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,56 +4,266 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
 
+const follow = require('./api/follow');
+
+const root = '/api';
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {rooms: []};
+        this.state = {rooms: [], attributes: [], pageSize: 2, links: {}};
+        this.updatePageSize = this.updatePageSize.bind(this);
+        this.onCreate = this.onCreate.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.onNavigate = this.onNavigate.bind(this);
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [{rel: 'rooms', params: {size: pageSize}}])
+        .then(roomCollection => {
+            return client({
+                    method: 'GET',
+                    path: roomCollection.entity._links.profile.href,
+                    headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return roomCollection;
+            });
+        }).done(roomCollection => {
+            this.setState({
+                rooms: roomCollection.entity._embedded.rooms,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: roomCollection.entity._links
+            });
+        });
+    }
+
+    onCreate(newRoom) {
+        follow(client, root, ['rooms'])
+            .then(roomCollection => {
+                return client({
+                    method: 'POST',
+                    path: roomCollection.entity._links.self.href,
+                    entity: newRoom,
+                    headers: {'Content-Type': 'application/json'}
+                });
+            }).then(response => {
+            return follow(client, root, [{rel: 'rooms', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            if (typeof response.entity._links.last !== "undefined") {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
+    }
+
+    onDelete(room) {
+        client({
+            method: 'DELETE',
+            path: room._links.self.href
+        }).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+
+    onNavigate(navUri) {
+        client({
+            method: 'GET',
+            path: navUri
+        }).done(roomCollection => {
+            this.setState({
+                rooms: roomCollection.entity._embedded.rooms,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: roomCollection.entity._links
+            });
+        });
+    }
+
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
     }
 
     componentDidMount() {
-        client({method: 'GET', path: '/api/rooms'}).done(response => {
-            this.setState({rooms: response.entity._embedded.rooms});
-        });
+        this.loadFromServer(this.state.pageSize);
     }
 
     render() {
         return (
-            <RoomList rooms={this.state.rooms}/>
+            <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <RoomList rooms={this.state.rooms}
+                          links={this.state.links}
+                          pageSize={this.state.pageSize}
+                          onNavigate={this.onNavigate}
+                          onDelete={this.onDelete}
+                          updatePageSize={this.updatePageSize}/>
+            </div>
+        )
+    }
+}
+
+class CreateDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const newRoom = {};
+        this.props.attributes.forEach(attribute => {
+            newRoom[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newRoom);
+
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        window.location = "#";
+    }
+
+    render() {
+        const inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field"/>>
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createRoom">Create</a>
+
+                <div id="createRoom" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Create new room</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
 
 class RoomList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNavFirst = this.handleNavFirst.bind(this);
+        this.handleNavPrev = this.handleNavPrev.bind(this);
+        this.handleNavNext = this.handleNavNext.bind(this);
+        this.handleNavLast = this.handleNavLast.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(e) {
+        e.preventDefault();
+        const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            ReactDOM.findDOMNode(this.refs.pageSize).value = pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+
+    handleNavFirst(e){
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
+    }
+
+
     render() {
         const rooms = this.props.rooms.map(room =>
-            <Room key={room._links.self.href} room={room}/>
+            <Room key={room._links.self.href} room={room} onDelete={this.props.onDelete}/>
         );
+
+        const navLinks = [];
+        if ("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if ("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        }
+        if ("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        }
+        if ("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        }
+
         return (
-            <table>
-                <tbody>
-                <tr>
-                    <th>title</th>
-                </tr>
-                {rooms}
-                </tbody>
-            </table>
+            <div>
+                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Title</th>
+                        <th></th>
+                    </tr>
+                    {rooms}
+                    </tbody>
+                </table>
+                <div>
+                    {navLinks}
+                </div>
+            </div>
         )
     }
+
 }
 
 class Room extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.room);
+    }
+
     render() {
         return (
             <tr>
                 <td>{this.props.room.title}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
         )
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('react')
 )
 
